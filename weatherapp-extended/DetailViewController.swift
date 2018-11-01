8c30febe0a001936d57193d90e68e8c16e20b9ce
Yurii Piets <yurii.piets@gmail.com>
2018-11-01 12:03:36 -0700

    add details view

--- weatherapp-extended/DetailViewController.swift
+++ weatherapp-extended/DetailViewController.swift
@@ -1,40 +1,90 @@
-//
-//  DetailViewController.swift
-//  weatherapp-extended
-//
-//  Created by plague on 11/1/18.
-//  Copyright © 2018 student. All rights reserved.
-//
-
 import UIKit
 
 class DetailViewController: UIViewController {
 
-    @IBOutlet weak var detailDescriptionLabel: UILabel!
-
-
+    @IBOutlet weak var cityNameLabel: UINavigationItem!
+    @IBOutlet weak var minTempLabel: UILabel!
+    @IBOutlet weak var maxTempLabel: UILabel!
+    @IBOutlet weak var windSpeedLabel: UILabel!
+    @IBOutlet weak var windDirLabel: UILabel!
+    @IBOutlet weak var himidityLabel: UILabel!
+    @IBOutlet weak var airPressureLabel: UILabel!
+    @IBOutlet weak var dateLabel: UILabel!
+    @IBOutlet weak var prevBtn: UIButton!
+    @IBOutlet weak var nextBtn: UIButton!
+    @IBOutlet weak var weatherIcon: UIImageView!
+    
+    var currentWeatherId:Int = 0
+    
     func configureView() {
-        // Update the user interface for the detail item.
         if let detail = detailItem {
-            if let label = detailDescriptionLabel {
-                label.text = detail.weather?.title
-            }
+            self.cityNameLabel.title = detail.weather!.title;
+            self.dateLabel.text = detail.weather!.weatherElements![self.currentWeatherId].applicableDate;
+            self.minTempLabel.text = String(format:"%.0f ºC", detail.weather!.weatherElements![self.currentWeatherId].minTemp!)
+            self.maxTempLabel.text = String(format:"%.0f ºC", detail.weather!.weatherElements![self.currentWeatherId].maxTemp!)
+            self.windSpeedLabel.text = String(format:"%.0f km/h", detail.weather!.weatherElements![self.currentWeatherId].windSpeed!)
+            self.windDirLabel.text = detail.weather!.weatherElements![self.currentWeatherId].windDirectionCompass!
+            self.himidityLabel.text = "\(detail.weather!.weatherElements![self.currentWeatherId].humidity!)" + "%"
+            self.airPressureLabel.text = String(format:"%.0f", detail.weather!.weatherElements![self.currentWeatherId].airPressure!) + " mbar"
         }
     }
 
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
-        configureView()
+        self.prevBtn.isEnabled = false
+        self.configureView()
+        self.initTargets()
+        self.loadIcon()
     }
 
     var detailItem: CityWeatherItem? {
         didSet {
-            // Update the view.
-            configureView()
         }
     }
+    
+    func initTargets(){
+        self.prevBtn.addTarget(self, action: #selector(prevBtnClicked), for: UIControl.Event.touchUpInside)
+        self.nextBtn.addTarget(self, action: #selector(nextBtnClicked), for: UIControl.Event.touchUpInside)
+    }
 
-
+    @objc func prevBtnClicked(sender: UIButton!){
+        self.currentWeatherId -= 1
+        self.configureView()
+        self.loadIcon()
+        if(self.currentWeatherId <= 0) {
+            self.prevBtn.isEnabled = false
+        }
+        self.nextBtn.isEnabled = true
+    }
+    
+    @objc func nextBtnClicked(sender: UIButton!){
+        self.currentWeatherId += 1
+        self.configureView()
+        self.loadIcon()
+        if(self.currentWeatherId >= detailItem!.weather!.weatherElements!.count - 1) {
+            self.nextBtn.isEnabled = false
+        }
+        self.prevBtn.isEnabled = true
+    }
+    
+    func loadIcon(){
+        let abbr = detailItem!.weather!.weatherElements![self.currentWeatherId].weatherStateAbbr!;
+        let iconUrl = "https://www.metaweather.com/static/img/weather/png/64/" + abbr + ".png"
+        
+        let url = URL(string: iconUrl);
+        let request: URLRequest = URLRequest(url: url!);
+        URLSession.shared.dataTask(with: request) {(d, resp, err) in
+            
+            if let err = err {
+                print("Unexpected \(err)");
+                return;
+            }
+            
+            DispatchQueue.main.async {
+                self.weatherIcon.image = UIImage(data: d!)
+            }
+            }.resume();
+    }
 }
 
